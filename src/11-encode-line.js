08f941c0848533d23e587baa572d57bcfb4c54db
kagerka <kagerka@gmail.com>
2021-04-08 09:59:04 +0300

    feat: add solution for js task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,23 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  let result = [];
+  let num = 0;
+  for (let i = 0; i <= arr.length; i++) {
+    num++;
+    if (arr[i] !== arr[i + 1] && arr[i] !== arr[i - 1]) {
+      result.push(arr[i]);
+      num = 0;
+    }
+    if (arr[i] !== arr[i + 1] && arr[i] === arr[i - 1]) {
+      result.push(num + arr[i]);
+      num = 0;
+    }
+  }
+  result = result.join('');
+  return result;
 }
 
 module.exports = encodeLine;
