646202079eb2489a12e128b6bae2d28e3af9c7a8
kagerka <kagerka@gmail.com>
2021-04-12 10:11:34 +0300

    feat: add solution for js task

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,37 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
-}
+function getDNSStats(domains) {
+  const arr = [];
+  let arr1 = [];
+  const arr2 = [];
+  const result = {};
+  let domain = '';
+  const count = 1;
 
+  for (let i = 0; i < domains.length; i++) {
+    arr[i] = domains[i].split('.');
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = (arr[i].length - 1); j >= 0; --j) {
+      arr1.push(`.${arr[i][j]}`);
+    }
+    arr2[i] = arr1;
+    arr1 = [];
+  }
+
+  for (let i = 0; i < arr2.length; i++) {
+    for (let j = 0; j < arr2[i].length; j++) {
+      domain += arr2[i][j];
+      if (domain in result) {
+        result[domain] = count + 1;
+      } else {
+        result[domain] = count;
+      }
+    }
+    domain = '';
+  }
+  return result;
+}
 module.exports = getDNSStats;
